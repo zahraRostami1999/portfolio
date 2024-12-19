8e3af7a37341e9029f4ff83a0337b44bc04a81a7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-19 17:04:31 +0330

    edit NavBar: remove w-11/12

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <>
-            <div className='flex w-11/12 justify-between'>
+            <div className='flex justify-between'>
                 <Link to="/about">About Me</Link>
                 <Link to="/skill">Skills</Link>
                 <Link to="/project">Projects</Link>
