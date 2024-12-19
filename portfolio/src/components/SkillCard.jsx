fc185cdcba288ec4c8cc95672df5b8a18ea4158a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-19 11:27:39 +0330

    edit SkillCard: add logo and Title part

---
+++ portfolio/src/components/SkillCard.jsx
@@ -0,0 +1,12 @@
+const SkillCard = () => {
+    return (
+        <>
+            <div className="w-40 h-40 border border-black rounded px-5 py-3">
+                <div>Logo</div>
+                <div>Title</div>
+            </div>
+        </>
+    );
+}
+
+export default SkillCard;
