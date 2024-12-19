e89e812586ca92ca55bc54a750b6465b810f3539
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-19 09:10:52 +0330

    edit App: add Routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutMePage from './pages/AboutMePage';
+import ContactMePage from './pages/ContactMePage';
+import SkillsPage from './pages/SkillsPage';
+import ProjectsPage from './pages/ProjectsPage';
+import Header from './components/Header';
 
 function App() {
 
 
   return (
     <>
-        <div>
-            Hello
-        </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutMePage />} />
+          <Route path="/contact" element={<ContactMePage />} />
+          <Route path="/project" element={<ProjectsPage />} />
+          <Route path="/skill" element={<SkillsPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
