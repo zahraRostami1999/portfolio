e5f5824e5162830f93686e2290186e264816c4cb
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-19 11:28:16 +0330

    edit Social Btn: add linkedin and telegram

---
+++ portfolio/src/components/SocialBtn.jsx
@@ -0,0 +1,20 @@
+const SocialBtn = () => {
+    return ( 
+        <>
+        <div className="w-1/5 flex justify-between">
+            <div>
+                <button className="bg-white px-5 py-2 border border-black rounded">
+                    T
+                </button>
+            </div>
+            <div>
+                <button className="bg-white px-5 py-2 border border-black rounded">
+                    L
+                </button>
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default SocialBtn;
