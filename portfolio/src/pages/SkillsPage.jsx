f0f261f7b05dcef8e6cb097547928eed6204c994
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-20 20:04:48 +0330

    edit SkillsPage: make responsive

--- portfolio/src/pages/SkillsPage.jsx
+++ portfolio/src/pages/SkillsPage.jsx
@@ -3,12 +3,12 @@ const SkillsPage = () => {
     return (
         <>
             <div className="w-full min-h-screen my-10">
-                <div className="container">
+                <div className="px-10">
                     <div className="text-5xl text-center my-5">
                         <h1>My Skills</h1>
                     </div>
-                    <div className="w-full my-20">
-                        <div className="grid grid-cols-4 gap-32 w-11/12 mx-auto">
+                    <div className="w-full md:my-20 sm:my-10 flex justify-center">
+                        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
                             <SkillCard />
                             <SkillCard />
                             <SkillCard />
@@ -16,7 +16,7 @@ const SkillsPage = () => {
                             <SkillCard />
                             <SkillCard />
                             <SkillCard />
-                            <SkillCard /> 
+                            <SkillCard />
                         </div>
                     </div>
                 </div>
