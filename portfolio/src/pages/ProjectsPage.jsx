3b0e3d710f30cb6a066d264dd4c24c3c284c17d0
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-20 20:04:25 +0330

    edit ProjectPage: remove container class

--- portfolio/src/pages/ProjectsPage.jsx
+++ portfolio/src/pages/ProjectsPage.jsx
@@ -2,8 +2,8 @@ import ProjectCard from "../components/ProjectCard";
 const ProjectsPage = () => {
     return (
         <>
-            <div>
-                <div className="my-10 container">
+            <div className="w-full min-h-screen">
+                <div className="my-10 px-10">
                     <div className="text-5xl text-center my-5">
                         <h1>My Projects</h1>
                     </div>
