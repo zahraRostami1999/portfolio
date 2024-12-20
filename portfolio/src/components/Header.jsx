876a40914345069be2bf199888a11c01d1e00063
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-20 20:01:37 +0330

    edit Header: sort code and add burger bar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,48 +1,34 @@
 import NavBar from "./NavBar";
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
+import BurgerBar from "./BurgerBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+        setIsMenuOpen(isMenuOpen => !isMenuOpen);
     }
 
     return (
         <>
-            <div className="flex justify-center w-full">
-                <div className="flex w-11/12 justify-between">
-                    <div className="w-2/3 flex justify-between">
-                        <div className="w-1/12 ">
-                            <Link to="/">logo</Link>
-                        </div>
-                        <div className="w-1/2">
-                            <NavBar />
-                        </div>
-                    </div>
-
-                    <div className="w-1/12 flex justify-end xl:hidden" onClick={()=>toggleMenu()}><FontAwesomeIcon icon={faBars} /></div>
-                    <div>
-                        <div className={`w-full ${isMenuOpen? "block" : "hidden"} flex justify-center items-center py-5`}>
-                            <div className="w-1/3">
-                                <Link to="/about">About Me</Link>
-                            </div>
-                            <div className="w-1/3">
-                                <Link to="/skill">Skills</Link>
-                            </div>
-                            <div className="w-1/3">
-                                <Link to="/project">Projects</Link>
-                            </div>
-                            <div className="w-1/3">
-                                <Link to="/contact">Contact Me</Link>
-                            </div>
-                        </div>
-                    </div>
+            <div className="flex justify-between items-center text-black py-3 px-8 md:px-20 lg:px-8 w-full xl:w-2/3 lg:w-5/6 md:w-5/6 drop-shadow-md">
+                <div>
+                    <Link to="/">Logo</Link>
                 </div>
+                <div className="hidden xl:flex lg:flex md:flex items-center gap-12 font-semibold text-base">
+                    <NavBar />
+                </div>
+                <div onClick={toggleMenu} className="xl:hidden lg:hidden md:hidden block text-xl">
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+                <div className={`xl:hidden top-12 left-0 w-full absolute flex flex-col items-center gap-7 text-base font-semibold transform transition-transform ${isMenuOpen ? "block" : "hidden"} md:hidden lg:hidden xl:hidden list-none bg-white`}>
+                    <BurgerBar />
+                </div>
+
             </div>
         </>
     );
