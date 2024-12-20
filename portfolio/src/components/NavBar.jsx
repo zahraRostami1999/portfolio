554883f6aed7f64cb37a787f1442fa9b7b93473e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-20 20:02:27 +0330

    edit NavBar

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -3,10 +3,12 @@ const NavBar = () => {
     return (
         <>
             <div className='flex justify-between'>
-                <Link to="/about">About Me</Link>
-                <Link to="/skill">Skills</Link>
-                <Link to="/project">Projects</Link>
-                <Link to="/contact">Contact Me</Link>
+                <ul className="flex justify-between list-none text-right mx-auto">
+                    <li className='px-5 cursor-pointer'><Link to="/about">About Me</Link></li>
+                    <li className='px-5 cursor-pointer'><Link to="/skill">Skills</Link></li>
+                    <li className='px-5 cursor-pointer'><Link to="/project">Projects</Link></li>
+                    <li className='px-5 cursor-pointer'><Link to="/contact">Contact Me</Link></li>
+                </ul>
             </div>
         </>
     );
