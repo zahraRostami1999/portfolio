2334aab0f1fc919ab2fa9d18bbc406e1a4b76206
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-20 20:02:54 +0330

    edit SkillCard: reove bg

--- portfolio/src/components/SkillCard.jsx
+++ portfolio/src/components/SkillCard.jsx
@@ -1,7 +1,7 @@
 const SkillCard = () => {
     return (
         <>
-            <div className="w-40 h-40 border border-black rounded px-5 py-3">
+            <div className="w-40 h-40 border border-black rounded px-5 py-3 bg-pink-500">
                 <div>Logo</div>
                 <div>Title</div>
             </div>
