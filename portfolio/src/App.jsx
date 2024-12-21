3134b0cea1bbc788e96dd031f8244951aa43281c
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-21 10:49:59 +0330

    edit App: remove about, skill, contact Routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,24 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AboutMePage from './pages/AboutMePage';
-import ContactMePage from './pages/ContactMePage';
-import SkillsPage from './pages/SkillsPage';
 import ProjectsPage from './pages/ProjectsPage';
 import Header from './components/Header';
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutMePage />} />
-          <Route path="/contact" element={<ContactMePage />} />
           <Route path="/project" element={<ProjectsPage />} />
-          <Route path="/skill" element={<SkillsPage />} />
         </Routes>
       </BrowserRouter>
     </>
