9fac00666f7bc64611d2b4f2f55c7db84226ad09
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-21 10:51:18 +0330

    edit NavBar: remove skill, about, contact links

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -4,10 +4,7 @@ const NavBar = () => {
         <>
             <div className='flex justify-between'>
                 <ul className="flex justify-between list-none text-right mx-auto">
-                    <li className='px-5 cursor-pointer'><Link to="/about">About Me</Link></li>
-                    <li className='px-5 cursor-pointer'><Link to="/skill">Skills</Link></li>
                     <li className='px-5 cursor-pointer'><Link to="/project">Projects</Link></li>
-                    <li className='px-5 cursor-pointer'><Link to="/contact">Contact Me</Link></li>
                 </ul>
             </div>
         </>
