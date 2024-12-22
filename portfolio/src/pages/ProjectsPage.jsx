5195180339919e306d4ff5be4dddc89e92f33d84
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 13:43:59 +0330

    edit Project Page: make responsive and add filter btns

--- portfolio/src/pages/ProjectsPage.jsx
+++ portfolio/src/pages/ProjectsPage.jsx
@@ -1,21 +1,37 @@
 import ProjectCard from "../components/ProjectCard";
+import { Projects } from "../data/projects";
+import { useState } from "react";
 const ProjectsPage = () => {
+    const [projectInfo, setProjectInfo] = useState([]);
+    const filterBtn = ["All", "React", "Tailwind CSS", "Redux"];
+    const [filter, setFilter] = useState("All");
+
+    // const handleFilterProducts = (newFilter) => {
+    //     setFilter(newFilter);
+    // };
+
+    // const filteredProducts = useMemo(() => {
+    //     if (filter === "all") return projectInfo;
+    //     return projectInfo.filter((project) => project.technologies === filter);
+    // }, [projectInfo, filter]);
+
     return (
         <>
-            <div className="w-full min-h-screen">
-                <div className="my-10 px-10">
-                    <div className="text-5xl text-center my-5">
-                        <h1>My Projects</h1>
+            <div className="w-full flex justify-center lg:py-14 md:py-14 sm:py-10 py-10">
+                <div className="w-11/12 flex justify-center flex-wrap ">
+                    <div className="w-full text-center mb-5 text-4xl font-bold">My Projects</div>
+                    <div className="w-full flex flex-row border-b justify-start text-xs lg:text-base md:text-base font-Poppins font-medium">
+                        {filterBtn.map((btn) => {
+                            return <button className="px-2 lg:px-5 md:px-3 py-2  hover:text-neutral-600 hover:border-b hover:border-neutral-900 transition-all duration-300" key={btn} onClick={() => handleFilterProducts(btn)}>{btn}</button>
+                        })}
                     </div>
-                    <div className="bg-black text-white">
-                        <div>
-                            <ProjectCard />
-                            <ProjectCard />
-                            <ProjectCard />
-                        </div>
-
+                    <div className="">
+                        {Projects.map(project =>
+                            <ProjectCard key={project.id} project={project} />
+                        )}
                     </div>
                 </div>
+
             </div>
         </>
     );
