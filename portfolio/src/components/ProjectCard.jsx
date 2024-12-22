b4d98fde01115618ab1568968fa5622944ea26c9
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 20:36:57 +0330

    edit Projectcard: make responsive

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -5,15 +5,16 @@ import img from '../assets/shop-cover.png';
 const ProjectCard = (props) => {  
     return (
         <>
-            <div className="w-full">
+            <div className="w-full border-b">
                 <div className="w-full flex flex-row justify-center py-10">
-                    <div className="w-1/2 flex justify-center">
-                        <img className="lg:w-2/3 md:w-4/5 sm:w-5/6 w-full" src={img} alt="project-image" />
+                    <div className="lg:w-1/2 w-5/6  flex justify-center md:w-2/3 sm:w-4/5">
+                        <img className="lg:w-2/3 md:w-4/5 sm:w-5/6 w-full " src={img} alt="project-image" />
                     </div>
-                    <div className="w-1/3 py-10 font-semibold flex justify-start items flex-col gap-3">
+                    <div className="w-1/3 py-10 font-semibold flex justify-start items flex-col gap-3 text-sm sm:text-sm md:text-lg">
                         <h1>{props.project.id}</h1>
                         <h3>{props.project.title}</h3>
-                        <button className="border-2 border-neutral-700 rounded w-1/2 px-5 py-1 lg:my-5 md:my-5 sm:my-3 my-3"><Link to={`/${props.project.id}`}>Details</Link></button>                      
+                        <h2>{props.project.date}</h2>
+                        <button className="hover:border-2 border-2 border-red-300 hover:border-neutral-700 hover:font-bold rounded lg:w-1/2 md:w-1/2 sm:w-5/6 w-full px-5 py-2 lg:my-5 md:my-5 sm:my-3 my-3 bg-red-300 text-neutral-800 sm:text-sm text-xs md:text-base"><Link to={`/${props.project.id}`}>Details</Link></button>                      
                     </div>
                 </div>
             </div>
