fe2a065c03a315754eefd634030d0744ac7ca9f7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 13:40:51 +0330

    edit Header: set fixed and remove burger bar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -2,7 +2,6 @@ import NavBar from "./NavBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
-import BurgerBar from "./BurgerBar";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -15,20 +14,13 @@ const Header = () => {
 
     return (
         <>
-            <div className="flex justify-between items-center text-black py-3 px-8 md:px-20 lg:px-8 w-full xl:w-2/3 lg:w-5/6 md:w-5/6 drop-shadow-md">
+            <div className="flex fixed justify-between items-center text-black py-3 px-8 md:px-20 lg:px-8 w-full xl:w-1/6 lg:w-1/3 md:w-5/6 drop-shadow-md">
                 <div>
                     <Link to="/">Logo</Link>
                 </div>
-                <div className="hidden xl:flex lg:flex md:flex items-center gap-12 font-semibold text-base">
+                <div className="font-semibold text-base">
                     <NavBar />
                 </div>
-                <div onClick={toggleMenu} className="xl:hidden lg:hidden md:hidden block text-xl">
-                    <FontAwesomeIcon icon={faBars} />
-                </div>
-                <div className={`xl:hidden top-12 left-0 w-full absolute flex flex-col items-center gap-7 text-base font-semibold transform transition-transform ${isMenuOpen ? "block" : "hidden"} md:hidden lg:hidden xl:hidden list-none bg-white`}>
-                    <BurgerBar />
-                </div>
-
             </div>
         </>
     );
