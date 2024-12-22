d1c2fdfae17f8ee8e792eaae6137b5bef6a286af
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 20:38:48 +0330

    edit ProjectPage: make responsive

--- portfolio/src/pages/ProjectsPage.jsx
+++ portfolio/src/pages/ProjectsPage.jsx
@@ -21,9 +21,9 @@ const ProjectsPage = () => {
     
     return (
         <>
-            <div className="w-full flex justify-center lg:py-14 md:py-14 sm:py-10 py-10">
+            <div className="w-full flex justify-center lg:py-3 md:py-14 sm:py-10 py-5 text-neutral-800">
                 <div className="w-11/12 flex justify-center flex-wrap ">
-                    <div className="w-full text-center mb-5 text-4xl font-bold">My Projects</div>
+                    <div className="w-full text-center mb-5 lg:text-4xl md:text-2xl sm:text-xl text-base font-bold">My Projects</div>
                     <div className="w-full flex flex-row border-b justify-start text-xs lg:text-base md:text-base font-Poppins font-medium">
                         {filterBtn.map((btn) => {
                             return <button className="px-2 lg:px-5 md:px-3 py-2  hover:text-neutral-600 hover:border-b hover:border-neutral-900 transition-all duration-300" key={btn} onClick={() => handleFilterProducts(btn)}>{btn}</button>
