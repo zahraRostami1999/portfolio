55c9bc2bccd00a5f766425973a09690f9bd1bd9d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 13:42:50 +0330

    edit SocialBtn: add font awesome icons and set links

--- portfolio/src/components/SocialBtn.jsx
+++ portfolio/src/components/SocialBtn.jsx
@@ -1,17 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTelegram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 const SocialBtn = () => {
     return (
         <>
-            <div className="w-1/6 h-full flex justify-between gap-6">
-                <button className=" bg-white rounded">
-                    {/* <FontAwesomeIcon icon={faGithub} /> */}
+            <div className="w-1/5 h-full flex justify-between gap-6 xl:text-3xl lg:xl:text-3xl md:text-2xl sm:text-xl text-xl">
+                <button className="rounded">
+                    <Link to="https://github.com/zahraRostami1999">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </Link>
                 </button>
-                <button className="rounded bg-black">
-                    {/* <FontAwesomeIcon icon={faGithub} /> */}
+                <button className="rounded">
+                    <Link to="https://telegram.me/zahra_rtm">
+                        <FontAwesomeIcon icon={faTelegram} />
+                    </Link>
                 </button>
-                <button className="rounded bg-black">
-                    {/* <FontAwesomeIcon icon={faGithub} /> */}
+                <button className="rounded">
+                    <Link to="https://www.linkedin.com/in/zahra-rostami-4049b2327">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </Link>
                 </button>
             </div>
         </>
