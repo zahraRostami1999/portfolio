b694ba048b11b96beea6f8a618a8dbcc39546e6a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 20:36:38 +0330

    edit HomeIntroduction: make responsive

--- portfolio/src/components/HomeIntroduction.jsx
+++ portfolio/src/components/HomeIntroduction.jsx
@@ -6,15 +6,15 @@ const HomeIntroduction = () => {
         <>
             <div className="w-full h-full flex flex-wrap justify-start items-center sm:py-10 py-6">
                 <div className="lg:text-5xl md:text-3xl sm:text-2xl text-2xl w-full my-5">
-                    <div className="w-full text-center sm:text-left">Hello I'm <span className="font-extrabold">Zahra Rostami</span></div>
+                    <div className="w-full">Hello I'm <span className="font-extrabold">Zahra Rostami</span></div>
                 </div>
-                <div className="lg:text-xl md:text-lg sm:text-base text-sm py-5">
+                <div className="lg:text-xl md:text-lg sm:text-base text-sm py-5 md:py-0">
                     <div>
                     I'm a <span className="font-extrabold">Frontend Developer</span> with expertise in React, Redux, and Tailwind CSS, along with experience in modern libraries and frameworks. My focus is on building scalable, efficient, and visually appealing web applications that prioritize performance and usability. Explore my portfolio to see the solutions I’ve delivered.
                     </div>
                 </div>
-                <div className="w-full text-center text-xl font-semibold mx-auto"><Link to="/project" className="bg-yellow-400 py-3 rounded px-5">See My Projects</Link></div>
-                <div className="w-full my-5">
+                <div className="w-full lg:text-xl md:text-base sm:text-base text-sm font-semibold lg:mx-auto mx-0 my-10 md:my-6"><Link to="/project" className="bg-yellow-400 py-3 rounded px-5">See My Projects</Link></div>
+                <div className="w-full my-5 md:my-0">
                     <SocialBtn />
                 </div>
             </div>
