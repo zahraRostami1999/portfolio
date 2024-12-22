532b203462f57c75f7a6756d9c774e495090b84a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-22 20:36:12 +0330

    edit Header: remove extra code and add logo

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,23 +1,16 @@
-import NavBar from "./NavBar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars,faMoon } from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react";
+import { faMoon, faCloud } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
 const Header = () => {
-
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(isMenuOpen => !isMenuOpen);
-    }
-
     return (
         <>
-            <div className="flex fixed justify-between items-center text-black py-3 px-8 md:px-20 lg:px-8 w-full xl:w-1/6 lg:w-1/3 md:w-5/6 drop-shadow-md">
-                <div className="text-2xl text-yellow-300">
+            <div className="flex justify-between items-center text-black py-3 px-8 md:px-20 lg:px-8 w-full xl:w-1/6 lg:w-1/3 md:w-5/6 drop-shadow-md">
+                <div className="lg:text-3xl md:text-2xl sm:text-2xl text-xl">
                     <Link to="/">
-                    <FontAwesomeIcon icon={faMoon}/>
+                        <FontAwesomeIcon className='text-neutral-300' icon={faCloud} />
+                        <FontAwesomeIcon className='text-yellow-300' icon={faMoon} />
+                        <FontAwesomeIcon className='text-neutral-400' icon={faCloud} />
                     </Link>
                 </div>
             </div>
