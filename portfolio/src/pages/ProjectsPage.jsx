0799dde080af96a1d2bd42c2c63c5554ab2a4089
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-23 22:01:22 +0330

    edit ProjectsPage: add Responsive to filter btns

--- portfolio/src/pages/ProjectsPage.jsx
+++ portfolio/src/pages/ProjectsPage.jsx
@@ -3,7 +3,7 @@ import { Projects } from "../data/projects";
 import { useState, useMemo,useEffect } from "react";
 const ProjectsPage = () => {
     const [projectInfo, setProjectInfo] = useState([]);
-    const filterBtn = ["All", "React", "Tailwind CSS", "Redux"];
+    const filterBtn = ["All", "React", "Tailwind CSS", "Redux", "Responsive"];
     const [filter, setFilter] = useState("All");
 
     useEffect(() => {
@@ -24,7 +24,7 @@ const ProjectsPage = () => {
             <div className="w-full flex justify-center lg:py-3 md:py-14 sm:py-10 py-5 text-neutral-800">
                 <div className="w-11/12 flex justify-center flex-wrap ">
                     <div className="w-full text-center mb-5 lg:text-4xl md:text-2xl sm:text-xl text-base font-bold">My Projects</div>
-                    <div className="w-full flex flex-row border-b justify-start text-xs lg:text-base md:text-base font-Poppins font-medium">
+                    <div className="w-full flex flex-row border-b justify-start text-xs lg:text-lg md:text-base font-Poppins font-medium">
                         {filterBtn.map((btn) => {
                             return <button className="px-2 lg:px-5 md:px-3 py-2  hover:text-neutral-600 hover:border-b hover:border-neutral-900 transition-all duration-300" key={btn} onClick={() => handleFilterProducts(btn)}>{btn}</button>
                         })}
