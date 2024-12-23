1869fb9c496d4499dcc84c331d81f736c62378a7
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-23 22:19:08 +0330

    edit SingleProject: change title and desceription text size

--- portfolio/src/pages/SingleProjectPage.jsx
+++ portfolio/src/pages/SingleProjectPage.jsx
@@ -7,14 +7,14 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 const SingleProjectPage = () => {
     const { id } = useParams();
     const ProjectInfo = Projects.find(project => project.id == id);
-        
+
     return (
         <>
             <div className="w-full min-h-screen flex justify-center items-center flex-row text-neutral-800 ">
                 <div className="lg:w-1/2 md:w-5/6 flex justify-around lg:flex-row md:flex-row sm:flex-wrap flex-wrap py-5">
                     <div className="lg:w-2/3 sm:w-5/6 w-5/6 md:w-2/3 sm:5/6 border-2 border-black lg:py-10 text-center lg:px-10 px-1 rounded my-5 md:my-0 lg:my-0">
-                        <h1 className="font-bold lg:text-3xl md:text-2xl sm:xl mb-3">{ProjectInfo.title}</h1>
-                        <p className="lg:text-xl md:text-xl sm:text-sm">{ProjectInfo.description}</p>
+                        <h1 className="font-bold lg:text-3xl md:text-2xl sm:text-xl text-lg  mb-3">{ProjectInfo.title}</h1>
+                        <p className="lg:text-base md:text-base sm:text-sm text-xs">{ProjectInfo.description}</p>
                         <img className="my-5" src={ProjectInfo.img} alt="" />
                     </div>
                     <div className="lg:w-1/4 md:w-1/4 sm:w-5/6 w-5/6 flex lg:flex-col md:flex-col sm:flex-row flex-row justify-between lg:text-xl md:text-base sm:text-sm text-sm">
@@ -31,9 +31,8 @@ const SingleProjectPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="fixed top-20 left-10  bg-red-300 rounded-full px-3 py-2">
+                <div className="absolute top-14 left-7  bg-red-300 rounded-full px-3 py-2">
                     <Link to="https://zahra-rostami.ir/project"><FontAwesomeIcon icon={faArrowLeft} /></Link>
-                    
                 </div>
             </div>
         </>
