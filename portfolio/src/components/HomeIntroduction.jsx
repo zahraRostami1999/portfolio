2a6d69cc6ad10a2ccdb0cf45f7626ccfd8478a1f
zahraRostami1999 <zahrarostami137878@gmail.com>
2024-12-23 10:00:31 +0330

    edit HomeIntro: edit project lists Btn

--- portfolio/src/components/HomeIntroduction.jsx
+++ portfolio/src/components/HomeIntroduction.jsx
@@ -13,7 +13,7 @@ const HomeIntroduction = () => {
                     I'm a <span className="font-extrabold">Frontend Developer</span> with expertise in React, Redux, and Tailwind CSS, along with experience in modern libraries and frameworks. My focus is on building scalable, efficient, and visually appealing web applications that prioritize performance and usability. Explore my portfolio to see the solutions I’ve delivered.
                     </div>
                 </div>
-                <div className="w-full lg:text-xl md:text-base sm:text-base text-sm font-semibold lg:mx-auto mx-0 my-10 md:my-6"><Link to="/project" className="bg-yellow-400 py-3 rounded px-5">See My Projects</Link></div>
+                <div className="w-full lg:text-xl md:text-base sm:text-base text-sm font-semibold lg:mx-auto mx-0 my-10 md:my-6"><Link to="/project" className="bg-yellow-400 py-3 rounded px-5">What I've Created</Link></div>
                 <div className="w-full my-5 md:my-0">
                     <SocialBtn />
                 </div>
