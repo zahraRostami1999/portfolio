fa1e29d4d4b567e8972b51931369a2a62cc9d5b2
zahraRostami1999 <zahrarostami137878@gmail.com>
2024-12-23 10:01:02 +0330

    edit Project Card: add var id for link to single page

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import img from '../assets/shop-cover.png';
 
 const ProjectCard = (props) => {  
+    const id = props.project.id;
     return (
         <>
             <div className="w-full border-b">
@@ -14,7 +15,7 @@ const ProjectCard = (props) => {
                         <h1>{props.project.id}</h1>
                         <h3>{props.project.title}</h3>
                         <h2>{props.project.date}</h2>
-                        <button className="hover:border-2 border-2 border-red-300 hover:border-neutral-700 hover:font-bold rounded lg:w-1/2 md:w-1/2 sm:w-5/6 w-full px-5 py-2 lg:my-5 md:my-5 sm:my-3 my-3 bg-red-300 text-neutral-800 sm:text-sm text-xs md:text-base"><Link to={`/${props.project.id}`}>Details</Link></button>                      
+                        <button className="hover:border-2 border-2 border-red-300 hover:border-neutral-700 hover:font-bold rounded lg:w-1/2 md:w-1/2 sm:w-5/6 w-full px-5 py-2 lg:my-5 md:my-5 sm:my-3 my-3 bg-red-300 text-neutral-800 sm:text-sm text-xs md:text-base"><Link to={`/project/${id}`}>Details</Link></button>                      
                     </div>
                 </div>
             </div>
