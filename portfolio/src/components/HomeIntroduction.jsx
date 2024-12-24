eb8f4254aa4a6f3bbf6c467d4d5c8d5891efae7d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-24 12:16:47 +0330

    edit HomeIntro: change introduction text

--- portfolio/src/components/HomeIntroduction.jsx
+++ portfolio/src/components/HomeIntroduction.jsx
@@ -10,7 +10,7 @@ const HomeIntroduction = () => {
                 </div>
                 <div className="lg:text-xl md:text-lg sm:text-base text-sm py-5 md:py-0">
                     <div>
-                    I'm a <span className="font-extrabold">Frontend Developer</span> with expertise in React, Redux, and Tailwind CSS, along with experience in modern libraries and frameworks. My focus is on building scalable, efficient, and visually appealing web applications that prioritize performance and usability. Explore my portfolio to see the solutions I’ve delivered.
+                    I'm a <span className="font-extrabold">Frontend Developer</span> who loves crafting awesome web experiences using React, Redux, and Tailwind CSS. I’m all about creating user-friendly, visually stunning, and high-performing web apps that make a difference. Check out my portfolio to see the cool stuff I’ve been working on!
                     </div>
                 </div>
                 <div className="w-full lg:text-xl md:text-base sm:text-base text-sm font-semibold lg:mx-auto mx-0 my-10 md:my-6"><Link to="/project" className="bg-yellow-400 py-3 rounded px-5">What I've Created</Link></div>
