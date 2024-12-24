6d496ce853f80748ec4c662e35602dd4a79b966d
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-24 12:17:04 +0330

    edit ProjectCard: remove projects id

--- portfolio/src/components/ProjectCard.jsx
+++ portfolio/src/components/ProjectCard.jsx
@@ -10,7 +10,6 @@ const ProjectCard = (props) => {
                         <img className="lg:w-2/5 md:w-4/5 sm:w-5/6 w-full " src={props.project.cover} alt="project-image" />
                     </div>
                     <div className="w-1/3 py-10 font-semibold flex justify-start items flex-col gap-3 text-sm sm:text-sm md:text-lg">
-                        <h1>{props.project.id}</h1>
                         <h3>{props.project.title}</h3>
                         <h2>{props.project.date}</h2>
                         <button className="hover:border-2 border-2 border-red-300 hover:border-neutral-700 hover:font-bold rounded lg:w-1/2 md:w-1/2 sm:w-5/6 w-full px-5 py-2 lg:my-5 md:my-5 sm:my-3 my-3 bg-red-300 text-neutral-800 sm:text-sm text-xs md:text-base"><Link to={`/project/${id}`}>Details</Link></button>                      
