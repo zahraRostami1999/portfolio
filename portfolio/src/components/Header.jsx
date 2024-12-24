4b535768dd234ad6c9bf4fce54eb0e81b963c35a
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-24 18:00:02 +0330

    edit Header: add home btn

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,19 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faCloud, faHome } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <>
-            <div className="flex fixed justify-between items-center text-black py-3 px-4 md:px-20 lg:px-8 w-full xl:w-1/6 lg:w-1/3 md:w-5/6 drop-shadow-md">
-                <div className="lg:text-3xl md:text-2xl sm:text-2xl text-xl">
-                    <Link to="/">
+            <div className="flex fixed justify-between items-center text-black py-3 px-4 md:px-20 lg:px-8 w-full drop-shadow-md">
+                <div className="lg:text-3xl md:text-2xl sm:text-2xl text-xl w-full flex justify-between">
+                    <div>
                         <FontAwesomeIcon className='text-neutral-300' icon={faCloud} />
                         <FontAwesomeIcon className='text-yellow-300' icon={faMoon} />
                         <FontAwesomeIcon className='text-neutral-400' icon={faCloud} />
+                    </div>
+                    <Link to="/">
+                        <div>
+                            <FontAwesomeIcon className='text-red-300' icon={faHome} />
+                        </div>
                     </Link>
                 </div>
-            </div>
+            </div >
         </>
     );
 }
